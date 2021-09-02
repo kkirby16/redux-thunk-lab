5f8bc419e2babd86ddc6004ad97c9588763c642f
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-01 21:52:01 -0700

    commit tree 50ee521aeaac36879a74d6b4e39cab5aeee73703

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,22 @@
 // write your CatList component here
+
+import React, { Component } from "react";
+
+class CatList extends Component {
+  //   componentDidMount() {
+  //     console.log("CATLISTPROPS:", this.props.catPics);
+  //   }
+
+  renderCats = () => {
+    return this.props.catPics.map((catPic) => {
+      return <img src={catPic.url}></img>;
+    });
+  };
+
+  render() {
+    console.log("CATLISTPROPS:", this.props.catPics);
+    return <div>{this.renderCats()}</div>;
+  }
+}
+
+export default CatList;
